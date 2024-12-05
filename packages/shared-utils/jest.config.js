c9fd5cd196efcf19b2fb0057976eaa60bb5f3403
Julius Simonelli <julius.simonelli@gmail.com>
2024-12-05 08:20:29 -0800

    test working

--- packages/shared-utils/jest.config.js
+++ packages/shared-utils/jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.(ts|tsx)$': ['ts-jest']
   }
 };
