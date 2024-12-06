f68b5b7addf8a00ebfb61642486579a1e8e743e2
Julius Simonelli <julius.simonelli@gmail.com>
2024-12-05 21:02:03 -0800

    rm jest configs

--- packages/shared-utils/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest']
-  }
-};
